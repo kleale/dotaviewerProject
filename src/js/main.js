6f1e2c23fcc02e8b3a69a18f557ee6caa68de99c
Alexey Kosolapov <klealekle@gmail.com>
2016-11-29 12:02:10 +0300

    project clear start

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,11 @@
 /*
  * Third party
  */
-//= ../assets/js/chosen.jquery.js
-//= ../assets/js/jquery.trackpad-scroll-emulator.min.js
-//= ../assets/js/screenfull.js
 //= ../assets/js/perfect-scrollbar.jquery.min.js
-
-//= ../assets/js/highcharts.js
-//= ../assets/js/highcharts-more.js
-//= ../assets/js/highcharts-exporting.js
-
-//= ../assets/js/jquery.countdown.min.js
-//= ../assets/js/bootstrap-switch.min.js
 //= ../assets/js/owl.carousel.min.js
-
-/* Experimental */
-//= ../assets/js/jquery.logosDistort.js
-//= ../assets/js/jquery.particleground.min.js
 
 /*
  * Custom
  */
 //= part/app.js
-//= part/match-page.js
-//= part/match-chart.init.js
+
